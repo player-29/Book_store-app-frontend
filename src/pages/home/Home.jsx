a917a2315acc26ffbe77f0e4ba2a5437aa904f63
ayushrathour <ayushrathour2910@gmail.com>
2024-12-20 14:07:26 +0530

    recommended changed to Recommended.jsx

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Hero from './Hero';
 import TopSellers from './TopSellers';
-import Recommended from './recommended';
+import Recommended from './Recommended.jsx';
 import News from './News';
 
 
